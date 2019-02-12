817282a168a40e39b47604af3f1a28ceb931f274
Haoran Sun <254664427@qq.com>
2019-02-12 19:31:05 +1100

    draw pictures
    Using matplotlib to draw pictures

--- DownloadDataset.py
+++ DownloadDataset.py
@@ -3,5 +3,5 @@
 import tushare as ts
 
 
-df = ts.get_hist_data('000001')
-df.to_csv('000001.csv')
+df = ts.get_hist_data('000003')
+df.to_csv('000003.csv')
--- draw_picture.py
+++ draw_picture.py
@@ -1,3 +1,19 @@
+import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
 
 
+stock1 = pd.read_csv('000001.csv')
+stock1['date'] = pd.to_datetime(stock1['date'])
 
+stock2 = pd.read_csv('000002.csv')
+stock2['date'] = pd.to_datetime(stock2['date'])
+
+plt.plot(stock1['date'], stock1['close'])
+plt.plot(stock2['date'],stock2['close'])
+plt.xticks(rotation=30)
+plt.xlabel('day')
+plt.ylabel('close price')
+plt.title('stock figure for 000002')
+
+plt.show()
